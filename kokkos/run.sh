04da7755c2579dd9a7e67666c87ba518cb95427b
Lam Nguyen <lamtnguyen989@gmail.com>
2026-01-05 06:04:32 -0600

    tinkering with Cmake compilations

--- kokkos/run.sh
+++ kokkos/run.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# Loading libraries with Spack
 spack load cmake kokkos
 
+# Cmake config
 BUILD_DIR=build
-cmake -B $BUILD_DIR --verbose
-make -C $BUILD_DIR VERBOSE=1
+cmake   -B $BUILD_DIR \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DKokkos_ENABLE_CUDA_RELOCATABLE_DEVICE_CODE=ON \
+
+# Compile and run executable
+make -C $BUILD_DIR VERBOSE=1 -j$(nproc)
 $BUILD_DIR/bitReversal > run.log
+
+# Result 
 cat run.log
